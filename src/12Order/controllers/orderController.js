f71c4df9cdd1d692651d32f7790538975601cb75
aphinet12T <aphinet.dam@onetwotrading.co.th>
2024-08-22 00:28:06 +0700

    Update orderController.js

--- src/12Order/controllers/orderController.js
+++ src/12Order/controllers/orderController.js
@@ -1,33 +1,125 @@
 const axiosInstance = require('../../utils/axiosInstance')
 const axios = require('axios')
 
+// exports.getOrderCm = async (req, res, next) => {
+//     try {
+//         const { status } = req.query
+//         console.log('Status:', status)
+//         if (!status) {
+//             return res.status(400).json({ message: "Status query parameter is required." });
+//         }
+//         const response = await axiosInstance.get('/order/getAll', {
+//             params: { status }
+//         })
+//         res.status(200).json(response.data)
+//     } catch (error) {
+//         console.error('Error fetching data from external API:', error)
+//         next(error); 
+//     }
+// }
 exports.getOrderCm = async (req, res, next) => {
     try {
         const { status } = req.query
         console.log('Status:', status)
         if (!status) {
-            return res.status(400).json({ message: "Status query parameter is required." });
+            return res.status(400).json({ message: "Status query parameter is required." })
         }
+
         const response = await axiosInstance.get('/order/getAll', {
             params: { status }
         })
-        res.status(200).json(response.data)
+
+        const orders = response.data
+
+        const stockData = []
+
+        const { data: warehouseStockData } = await axios.post('http://192.168.2.97:8383/M3API/StockManage/Stock/getStockAll', {
+            warehouse: '105'
+        })
+        stockData.push(...warehouseStockData)
+        console.log('1',stockData)
+
+        for (let order of orders) {
+            let send = true
+
+            const groupedItems = order.list.reduce((acc, item) => {
+                if (!acc[item.id]) {
+                    acc[item.id] = 0
+                }
+                acc[item.id] += item.qtyPcs
+                return acc
+            }, {})
+            console.log('2',groupedItems)
+
+            for (let itemId in groupedItems) {
+                const stockItem = stockData.find(stock => stock.itemcode === itemId)
+
+                if (stockItem) {
+                    const availableStock = stockItem.available
+                    const requiredQty = groupedItems[itemId]
+
+                    if (requiredQty > availableStock) {
+                        send = false
+                        break;
+                    }
+                } else {
+                    send = false
+                    break;
+                }
+            }
+            order.send = send
+        }
+
+        res.status(200).json(orders)
     } catch (error) {
         console.error('Error fetching data from external API:', error)
-        next(error); 
+        next(error)
     }
 }
+
+// exports.getOrderCmDetail = async (req, res, next) => {
+//     try {
+//         const { orderNo } = req.body
+//         const response = await axiosInstance.post('/order/getDetail', { orderNo })
+//         res.status(200).json([response.data])
+//     } catch (error) {
+//         console.error('Error fetching data from external API:', error)
+//         next(error)
+//     }
+// }
 
 exports.getOrderCmDetail = async (req, res, next) => {
     try {
         const { orderNo } = req.body
+
         const response = await axiosInstance.post('/order/getDetail', { orderNo })
-        res.status(200).json([response.data])
+        const orderDetail = response.data
+
+
+        const { data: stockData } = await axios.post('http://192.168.2.97:8383/M3API/StockManage/Stock/getStockAll', {
+            warehouse: '105'
+        })
+
+        for (let item of orderDetail.list) {
+            const stockItem = stockData.find(stock => stock.itemcode === item.id)
+
+            if (stockItem) {
+                const availableStock = stockItem.available
+                const requiredQtyPcs = item.qtyPcs
+
+                item.stock = requiredQtyPcs <= availableStock
+            } else {
+                item.stock = false
+            }
+        }
+
+        res.status(200).json([orderDetail])
     } catch (error) {
         console.error('Error fetching data from external API:', error)
-        next(error); 
+        next(error)
     }
-};
+}
+
 
 // exports.summaryOrder = async (req, res, next) => {
 //     try {
@@ -158,6 +250,57 @@ exports.getOrderCmDetail = async (req, res, next) => {
 //     return convertedUnits;
 // };
 
+// exports.summaryOrder = async (req, res, next) => {
+//     try {
+//         const orders = req.body
+
+//         const summarizedOrders = orders.map(order => {
+//             const summarizedList = [];
+
+//             for (let item of order.list) {
+//                 const convertedUnits = convertToUnits(item.qty, item.unitText)
+
+//                 const existingItem = summarizedList.find(i => i.id === item.id)
+
+//                 if (existingItem) {
+//                     existingItem.qtyPcs += item.qtyPcs
+//                     existingItem.convertedUnits = mergeConvertedUnits(existingItem.convertedUnits, convertedUnits)
+//                 } else {
+//                     const { id, name, qty, qtyPcs } = item
+//                     summarizedList.push({
+//                         id,
+//                         name,
+//                         qty,
+//                         qtyPcs,
+//                         convertedUnits
+//                     });
+//                 }
+//             }
+
+//             return {
+//                 orderNo: order.orderNo,
+//                 saleMan: order.saleMan,
+//                 saleCode: order.saleCode,
+//                 area: order.area,
+//                 storeId: order.storeId,
+//                 storeName: order.storeName,
+//                 address: order.address,
+//                 taxID: order.taxID,
+//                 tel: order.tel,
+//                 warehouse: order.warehouse,
+//                 note: order.note,
+//                 createDate: order.createDate,
+//                 list: summarizedList
+//             }
+//         })
+
+//         res.status(200).json(summarizedOrders)
+//     } catch (error) {
+//         console.error('Error summarizing orders:', error)
+//         next(error)
+//     }
+// }
+
 exports.summaryOrder = async (req, res, next) => {
     try {
         const orders = req.body;
@@ -165,24 +308,32 @@ exports.summaryOrder = async (req, res, next) => {
         const summarizedOrders = orders.map(order => {
             const summarizedList = [];
 
-            for (let item of order.list) {
-                const convertedUnits = convertToUnits(item.qty, item.unitText);
+            let totalLarge = 0
+            let totalMedium = 0
+            let totalSmall = 0
 
-                const existingItem = summarizedList.find(i => i.id === item.id);
+            for (let item of order.list) {
+                const convertedUnits = convertToUnits(item.qty, item.unitText)
+
+                const existingItem = summarizedList.find(i => i.id === item.id)
 
                 if (existingItem) {
-                    existingItem.qtyPcs += item.qtyPcs;
-                    existingItem.convertedUnits = mergeConvertedUnits(existingItem.convertedUnits, convertedUnits);
+                    existingItem.qtyPcs += item.qtyPcs
+                    existingItem.convertedUnits = mergeConvertedUnits(existingItem.convertedUnits, convertedUnits)
                 } else {
-                    const { id, name, qty, qtyPcs } = item;
+                    const { id, name, qty, qtyPcs } = item
                     summarizedList.push({
                         id,
                         name,
                         qty,
                         qtyPcs,
                         convertedUnits
-                    });
+                    })
                 }
+
+                totalLarge += convertedUnits.large.qty
+                totalMedium += convertedUnits.medium.qty
+                totalSmall += convertedUnits.small.qty
             }
 
             return {
@@ -198,51 +349,73 @@ exports.summaryOrder = async (req, res, next) => {
                 warehouse: order.warehouse,
                 note: order.note,
                 createDate: order.createDate,
-                list: summarizedList
-            };
-        });
+                list: summarizedList,
+                totalList: {
+                    large: totalLarge,
+                    medium: totalMedium,
+                    small: totalSmall
+                }
+            }
+        })
 
-        res.status(200).json(summarizedOrders);
+        res.status(200).json(summarizedOrders)
     } catch (error) {
-        console.error('Error summarizing orders:', error);
-        next(error);
+        console.error('Error summarizing orders:', error)
+        next(error)
     }
-};
+}
 
 exports.summaryAll = async (req, res, next) => {
     try {
-        const orders = req.body;
+        const orders = req.body
 
-        const summarizedList = [];
+        const summarizedList = []
+
+        let totalLarge = 0
+        let totalMedium = 0
+        let totalSmall = 0
 
         for (let order of orders) {
             for (let item of order.list) {
-                const convertedUnits = convertToUnits(item.qty, item.unitText);
+                const convertedUnits = convertToUnits(item.qty, item.unitText)
 
-                const existingItem = summarizedList.find(i => i.id === item.id);
+                const existingItem = summarizedList.find(i => i.id === item.id)
 
                 if (existingItem) {
                     existingItem.qtyPcs += item.qtyPcs;
-                    existingItem.convertedUnits = mergeConvertedUnits(existingItem.convertedUnits, convertedUnits);
+                    existingItem.convertedUnits = mergeConvertedUnits(existingItem.convertedUnits, convertedUnits)
                 } else {
-                    const { id, name, qty, qtyPcs } = item;
+                    const { id, name, qty, qtyPcs } = item
                     summarizedList.push({
                         id,
                         name,
                         qty,
                         qtyPcs,
                         convertedUnits
-                    });
+                    })
                 }
+
+                totalLarge += convertedUnits.large.qty
+                totalMedium += convertedUnits.medium.qty
+                totalSmall += convertedUnits.small.qty
             }
         }
 
-        res.status(200).json(summarizedList);
+        const response = {
+            list: summarizedList,
+            totalList: {
+                large: totalLarge,
+                medium: totalMedium,
+                small: totalSmall
+            }
+        }
+
+        res.status(200).json(response)
     } catch (error) {
-        console.error('Error summarizing all orders:', error);
-        next(error);
+        console.error('Error summarizing all orders:', error)
+        next(error)
     }
-};
+}
 
 const convertToUnits = (qty, unitText) => {
     const convertedUnits = {
@@ -255,12 +428,12 @@ const convertToUnits = (qty, unitText) => {
         convertedUnits.large.qty = qty
     } else if (['BAG', 'PAC', 'CRT'].includes(unitText)) {
         convertedUnits.medium.qty = qty
-    } else if (['PCS','BOT'].includes(unitText)) {
+    } else if (['PCS', 'BOT'].includes(unitText)) {
         convertedUnits.small.qty = qty
     }
 
-    return convertedUnits;
-};
+    return convertedUnits
+}
 
 const mergeConvertedUnits = (existingUnits, newUnits) => {
     return {
@@ -273,5 +446,5 @@ const mergeConvertedUnits = (existingUnits, newUnits) => {
         small: {
             qty: existingUnits.small.qty + newUnits.small.qty
         }
-    };
-};
+    }
+}
